62fb03abebdbe748e4632f1e12e2d0511bf0bea4
Eng-Prud <prudence.n826@gmaill.com>
2024-06-29 19:37:20 +0000

    Ammendthe syntax

---
+++ 0x00-pascal_triangle/.0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Pascal triangle
+"""
+
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+
+    # Initialize the first row of Pascal's triangle
+    pascal = [[1]]
+
+    for i in range(1, n):
+        row = [1]  # First element is always 1
+        for j in range(1, i):
+            # Each element is the sum of the two elements above it
+            row.append(pascal[i-1][j-1] + pascal[i-1][j])
+        row.append(1)  # Last element is always 1
+        pascal.append(row)
+
+    return pascal
--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
 """
-Pascal triangle
+Pascal's triangle
 """
 
 
-
 def pascal_triangle(n):
-    """
-    Returns a list of lists of integers representing the Pascal’s triangle of n
-    """
     if n <= 0:
         return []
-
-    # Initialize the first row of Pascal's triangle
-    pascal = [[1]]
-
-    for i in range(1, n):
-        row = [1]  # First element is always 1
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1, 1]]
+    triangle = [[1], [1, 1]]
+    for i in range(2, n):
+        row = [1]
         for j in range(1, i):
-            # Each element is the sum of the two elements above it
-            row.append(pascal[i-1][j-1] + pascal[i-1][j])
-        row.append(1)  # Last element is always 1
-        pascal.append(row)
-
-    return pascal
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
